4607a92047695916cdbf524fb0e4b81c9fe8ea03
jun <justdoit7724@gmail.com>
2020-02-20 16:20:42 +0900

    UI Scene implementing

---
+++ Assets/Scripts/LayoutContentModifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(HorizontalLayoutGroup))]
+public class LayoutContentModifier : MonoBehaviour
+{
+    void Start()
+    {
+        float spacing = GetComponent<HorizontalLayoutGroup>().spacing;
+
+        Button[] elems = GetComponentsInChildren<Button>();
+        float elemWidth = elems[0].GetComponent<RectTransform>().sizeDelta.x;
+
+        float panelWidth = (elemWidth + spacing) * elems.Length;
+
+        RectTransform curRectTransform = GetComponent<RectTransform>();
+        curRectTransform.sizeDelta = new Vector2(panelWidth, curRectTransform.sizeDelta.y);
+        curRectTransform.anchoredPosition3D = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct SelectInfo
+{
+    public RectTransform obj;
+    public RectTransform content;
+    public Image activeButtonImage;
+}
+
+public class UIController : MonoBehaviour
+{
+    public Sprite activeButtonSprite;
+    public Sprite unactiveButtonSprite;
+
+    public ScrollRect selectPanel;
+    public SelectInfo[] selections;
+
+    private int curSelectIdx = 0;
+
+    void Start()
+    {
+        SetNewSelect(curSelectIdx);
+    }
+
+    public void SetNewSelect(int idx)
+    {
+        curSelectIdx = idx;
+
+        foreach(var item in selections)
+        {
+            item.obj.gameObject.SetActive(false);
+            item.activeButtonImage.sprite = unactiveButtonSprite;
+        }
+        selectPanel.content = selections[curSelectIdx].content;
+        selectPanel.viewport = selections[curSelectIdx].obj;
+        selections[curSelectIdx].obj.gameObject.SetActive(true);
+        selections[curSelectIdx].activeButtonImage.sprite = activeButtonSprite;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1,LoadSceneMode.Additive);
+
+    }
+}
